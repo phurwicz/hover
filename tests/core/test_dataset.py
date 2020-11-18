f138358fd8ee69c52fb23ce1a337ea228710b65c
phurwicz <pavelhurwicz@gmail.com>
2020-11-17 23:02:00 -0500

    Added tests for hover.core.dataset and hover.core.explorer

---
+++ tests/core/test_dataset.py
@@ -0,0 +1,43 @@
+from hover.core.dataset import SupervisableTextDataset
+import pytest
+
+
+@pytest.mark.core
+class TestSupervisableTextDataset:
+    TEST_DICTL = [
+        {"content": "Aristotle", "mark": "A"},
+        {"content": "Bertrand Russell", "mark": "B"},
+        {"content": "CRISPR", "mark": "C"},
+        {"content": "Doudna", "mark": "D"},
+    ]
+    DEV_DICTL = [
+        {"content": "Doudna", "mark": "D"},
+        {"content": "Ernest Hemingway", "mark": "E"},
+    ]
+    TRAIN_DICTL = [
+        {"content": "Ernest Hemingway", "mark": "e"},
+        {"content": "Franklin Roosevelt", "mark": "F"},
+        {"content": "Geralt of Rivia", "mark": "G"},
+    ]
+    RAW_DICTL = [{"content": "Geralt of Rivia"}, {"content": "Hailstorm"}]
+
+    EFFECTIVE_SIZE = {"test": 4, "dev": 1, "train": 2, "raw": 1}
+
+    EFFECTIVE_CLASSES = 7
+
+    def test_init(self):
+        dataset = SupervisableTextDataset(
+            self.__class__.RAW_DICTL[:],
+            train_dictl=self.__class__.TRAIN_DICTL[:],
+            dev_dictl=self.__class__.DEV_DICTL[:],
+            test_dictl=self.__class__.TEST_DICTL[:],
+            text_key="content",
+            label_key="mark",
+        )
+
+        # check the subset sizes
+        for _key, _value in self.__class__.EFFECTIVE_SIZE.items():
+            assert dataset.dfs[_key].shape[0] == _value
+
+        # check the number of classes
+        assert len(dataset.label_encoder) == self.__class__.EFFECTIVE_CLASSES
---
+++ tests/core/test_explorer.py
@@ -0,0 +1,70 @@
+"""
+Note that the whole point of explorers is to allow interaction, for which this file should not be considered a complete suite of tests.
+"""
+
+from hover.core.explorer import (
+    BokehCorpusExplorer,
+    BokehCorpusAnnotator,
+    BokehMarginExplorer,
+    BokehSnorkelExplorer,
+)
+import pytest
+import pandas as pd
+import faker
+import random
+
+fake_en = faker.Faker('en')
+
+EXAMPLE_RAW_DF = pd.DataFrame(
+    [
+        {
+            "text": fake_en.paragraph(3),
+            "x": random.uniform(-1.0, 1.0),
+            "y": random.uniform(-1.0, 1.0),
+        } for i in range(300)
+    ],
+)
+
+EXAMPLE_MARGIN_DF = EXAMPLE_RAW_DF.copy()
+EXAMPLE_MARGIN_DF["label_1"] = EXAMPLE_MARGIN_DF["text"].apply(lambda x: random.choice(["A", "B"]))
+EXAMPLE_MARGIN_DF["label_2"] = EXAMPLE_MARGIN_DF["text"].apply(lambda x: random.choice(["A", "B"]))
+
+EXAMPLE_DEV_DF = pd.DataFrame(
+    [
+        {
+            "text": fake_en.paragraph(3),
+            "x": random.uniform(-1.0, 1.0),
+            "y": random.uniform(-1.0, 1.0),
+            "label": random.choice(["A", "B"])
+        } for i in range(100)
+    ]
+)
+
+
+
+@pytest.mark.core
+class TestBokehCorpusExplorer:
+    def test_init(self):
+        explorer = BokehCorpusExplorer(EXAMPLE_RAW_DF)
+        _ = explorer.view()
+
+@pytest.mark.core
+class TestBokehCorpusAnnotator:
+    def test_init(self):
+        explorer = BokehCorpusAnnotator(EXAMPLE_RAW_DF)
+        _ = explorer.view()
+        
+@pytest.mark.core
+class BokehMarginExplorer:
+    def test_init(self):
+        explorer = BokehMarginExplorer(EXAMPLE_MARGIN_DF, 'label_1', 'label_2')
+        explorer.plot("A")
+        explorer.plot("B")
+        _ = explorer.view()
+        
+@pytest.mark.core
+class TestBokehSnorkelExplorer:
+    def test_init(self):
+        #explorer = BokehSnorkelExplorer(EXAMPLE_RAW_DF, EXAMPLE_DEV_DF)
+        #_ = explorer.view()
+        pass
