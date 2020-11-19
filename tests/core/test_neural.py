1bf68aef5746a4e678e9154117a9f391819c52d0
phurwicz <pavelhurwicz@gmail.com>
2020-11-19 22:23:45 +0800

    Turned 'static' tests to static methods

--- tests/core/test_neural.py
+++ tests/core/test_neural.py
@@ -14,14 +14,17 @@ class TestVectorNet(object):
     For the VectorNet base class.
     """
 
-    def test_save(self, example_tvnet):
+    @staticmethod
+    def test_save(example_tvnet):
         default_path = example_tvnet.nn_update_path
         example_tvnet.save(f"{default_path}.test")
 
-    def test_adjust_optimier_params(self, example_tvnet):
+    @staticmethod
+    def test_adjust_optimier_params(example_tvnet):
         example_tvnet.adjust_optimizer_params()
 
-    def test_predict_proba(self, example_tvnet):
+    @staticmethod
+    def test_predict_proba(example_tvnet):
         proba_single = example_tvnet.predict_proba("hello")
         assert proba_single.shape[0] == 2
         proba_multi = example_tvnet.predict_proba(["hello", "bye", "ciao"])
