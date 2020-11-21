875a28d63fa4147eb57d98e429a6e67f41ec445a
phurwicz <pavelhurwicz@gmail.com>
2020-11-21 17:05:38 +0800

    Extended a submodule docstring

--- hover/core/representation/__init__.py
+++ hover/core/representation/__init__.py
@@ -1,3 +1,4 @@
 """
-Leveraging dimensionality reduction to make 2-D representations of data points.
+Leveraging dimensionality reduction to make 2-D representations of data points,
+which will be useful for making interactive general-purpose data explorers.
 """
