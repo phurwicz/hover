9ed432c0dd2c92d6065800ea71bb27ba06deb6da
phurwicz <pavelhurwicz@gmail.com>
2020-11-23 22:51:18 +0800

    Initialized a VisualAnnotation workflow

--- hover/recipe/__init__.py
+++ hover/recipe/__init__.py
@@ -3,14 +3,66 @@ High-level workflows.
 """
 import pandas as pd
 from tqdm import tqdm
+from hover import module_config
 from hover.core.dataset import SupervisableTextDataset
 from hover.core.neural import create_text_vector_net_from_module, TextVectorNet
 from hover.utils.torch_helper import vector_dataloader, one_hot, label_smoothing
+from .subroutine import link_plots
 from wasabi import msg as logger
 from wrappy import todo
 
 
 class VisualAnnotation:
+    """
+    Using a vectorizer followed by dimensionality reduction,
+    produce a 2-D visualization of the dataset, then annotate it.
+    """
+
+    def __init__(self, dataset, vectorizer):
+        """
+        """
+        assert isinstance(dataset, SupervisableDataset)
+        assert callable(vectorizer)
+        self.dataset = dataset
+        self.vectorizer = vectorizer
+        self.flush()
+        self.dataset.compute_2d_embedding(self.vectorizer, **kwargs)
+
+    @wrappy.todo("Review this function")
+    def flush(self, subset="train"):
+        """
+        Flush annotated pieces into train/dev/test, then re-create explorers.
+        """
+        # pick out the annotated slice
+        annotated_df = self.corpus_annotator.df_raw
+        annotated_slice = annotated_df[
+            annotated_df["label"] != module_config.ABSTAIN_DECODED
+        ]
+
+        # append annotated slice to the designated subset, then deduplicate and find classes
+        self.dataset.dfs[subset] = pd.concat(
+            [self.dataset.dfs[subset], annotated_slice], axis=0
+        )
+        self.dataset.df_deduplicate()
+        self.dataset.setup_label_coding()
+        self.dataset.synchronize_df_to_dictl()
+
+        # DF entries have now changed; reset the plots
+        self.corpus_explorer = BokehCorpusExplorer(
+            self.dataset.dfs["raw"], title="Corpus Explorer"
+        )
+        self.corpus_annotator = BokehCorpusAnnotator(
+            self.dataset.dfs["raw"], title="Corpus Annotator"
+        )
+
+    def plots(self, **kwargs):
+        """
+        A list of plots to be shown.
+        """
+        return link_plots(self.corpus_explorer, self.corpus_annotator, **kwargs)
+
+
+class TBD:
     """
     """
 
---
+++ hover/recipe/subroutine.py
@@ -0,0 +1,22 @@
+"""
+Functions commonly used by classes in this submodule.
+"""
+
+
+def link_plots(*explorers, height=600, width=800):
+    linked_plots = [*explorers]
+
+    for i, _pi in enumerate(linked_plots):
+        # set plots to the same size
+        _pi.figure.plot_height = height
+        _pi.figure.plot_width = width
+        for j, _pj in enumerate(linked_plots):
+            if not i == j:
+                # link coordinate ranges
+                for _attr in ["start", "end"]:
+                    _pi.figure.x_range.js_link(_attr, _pj.figure.x_range, _attr)
+                    _pi.figure.y_range.js_link(_attr, _pj.figure.y_range, _attr)
+                # link selection
+                _pi.source.selected.js_link("indices", _pj.source.selected, "indices")
+
+    return linked_plots
