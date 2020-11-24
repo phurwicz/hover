Bugfix: corrected imports; Tests: created a test for layerwise manifold calculation

hover/core/representation/manifold.py
@@ -3,8 +3,9 @@ Manifold similarity measures for any collection of sequences of vectors.
 Can be useful for improved interpretability of neural nets.
 """
 from .reduction import DimensionalityReducer
-from hover import module_params
+from hover import module_config
 from tqdm import tqdm
+from wasabi import msg as logger
 from scipy.spatial import procrustes
 
 DEFAULT_UMAP_PARAMS = {
@@ -30,7 +31,6 @@ class LayerwiseManifold(object):
         :param seq_arr: sequence of arrays to fit the manifold with.
         :type seq_arr: list of numpy.ndarrays.
         """
-        self.logger = module_params.default_logger()
         self.arrays = seq_arr[:]
         self.validate()
         self.standardize()
@@ -45,7 +45,7 @@ class LayerwiseManifold(object):
         self.n_vecs = self.arrays[0].shape[0]
         for _arr in self.arrays:
             assert _arr.shape[0] == self.n_vecs
-        self.logger.good("Validated dimensions of input arrays")
+        logger.good("Validated dimensions of input arrays")
 
     def standardize(self):
         """
@@ -59,7 +59,7 @@ class LayerwiseManifold(object):
             return matrix
 
         self.arrays = [transform(_arr) for _arr in self.arrays]
-        self.logger.good("Standardized input arrays")
+        logger.good("Standardized input arrays")
 
     def unfold(self, method="umap", reducer_kwargs=None):
         """
@@ -69,17 +69,17 @@ class LayerwiseManifold(object):
         """
         assert method in {"umap", "ivis"}
         if method == "umap":
-            reducer_kwargs = reducer_kwargs or DEFAULT_UMAP_KWARGS
+            reducer_kwargs = reducer_kwargs or DEFAULT_UMAP_PARAMS
         else:
             reducer_kwargs = reducer_kwargs or dict()
 
         self.manifolds = []
-        self.logger.info(f"Running {method}...")
+        logger.info(f"Running {method}...")
         for _arr in tqdm(self.arrays, total=len(self.arrays)):
             _reducer = DimensionalityReducer(_arr)
             _manifold = _reducer.fit_transform(method, **reducer_kwargs)
             self.manifolds.append(_manifold)
-        self.logger.good("Successfully unfolded arrays into manifolds")
+        logger.good("Successfully unfolded arrays into manifolds")
 
     def procrustes(self, arrays=None):
         """
@@ -100,5 +100,5 @@ class LayerwiseManifold(object):
             disparities.append(_disparity)
             fit_arrays.append(_matrix)
 
-        self.logger.good("Successfully carried out Procrustes analysis")
+        logger.good("Successfully carried out Procrustes analysis")
         return fit_arrays, disparities