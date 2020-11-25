Bugfix: SciPy's splprep breaks when two consecutive points are equal, and gets numerically unstable when they are almost equal. Now taking precaution by screening consecutive points and adding a noise when needed

hover/core/representation/trajectory.py
@@ -22,9 +22,18 @@ def spline(arr_per_dim, points_per_step=1, splprep_kwargs=None):
     num_given_points = arr_per_dim[0].shape[0]
     assert num_given_points > 1, "Need at least two points to fit a line"
 
+    # check if two vectors are almost identical, and apply a noise in that case
+    # note that we did not modify arr_per_dim in place
+    # and that the noise only goes up in a greedy random-walk manner
+    noise_arr = np.random.zeros(arr_per_dim.shape)
+    for i in range(1, num_given_points):
+        prev_vec, vec = arr_per_dim[:, i - 1] + noise_arr[:, i - 1], arr_per_dim[:, i]
+        while np.allclose(vec + noise_arr[:, i], prev_vec):
+            noise_arr[:, i] += np.random.uniform(low=-1e-6, high=1e-6, size=vec.shape)
+
     # reduce spline order if necessary, then fit the spline parameters
     splprep_kwargs["k"] = min(3, num_given_points - 1)
-    tck, u = interpolate.splprep(arr_per_dim, **splprep_kwargs)
+    tck, u = interpolate.splprep(arr_per_dim + noise_arr, **splprep_kwargs)
 
     # determine points at which the spline should be evaluated
     points_to_eval = []