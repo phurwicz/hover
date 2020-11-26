ef85924231eef8713ea27d838e15519ed6090400
phurwicz <pavelhurwicz@gmail.com>
2020-11-26 22:36:03 +0800

    Reorganized plot linking

--- hover/recipe/subroutine.py
+++ hover/recipe/subroutine.py
@@ -5,20 +5,30 @@ Note that functions which are also used outside this submodule should be moved u
 """
 
 
-def link_plots(*explorers, height=600, width=800):
-    linked_plots = [*explorers]
+def link_size_and_range(*figures, height=600, width=800):
+    """
+    Give the specified figures the same size and display range.
 
-    for i, _pi in enumerate(linked_plots):
+    This also synchronizes zooming in or out.
+    """
+    for i, _fi in enumerate(figures):
         # set plots to the same size
-        _pi.figure.plot_height = height
-        _pi.figure.plot_width = width
-        for j, _pj in enumerate(linked_plots):
+        _fi.plot_height = height
+        _fi.plot_width = width
+        for j, _fj in enumerate(figures):
             if not i == j:
                 # link coordinate ranges
                 for _attr in ["start", "end"]:
-                    _pi.figure.x_range.js_link(_attr, _pj.figure.x_range, _attr)
-                    _pi.figure.y_range.js_link(_attr, _pj.figure.y_range, _attr)
-                # link selection
-                _pi.source.selected.js_link("indices", _pj.source.selected, "indices")
+                    _fi.x_range.js_link(_attr, _fj.x_range, _attr)
+                    _fi.y_range.js_link(_attr, _fj.y_range, _attr)
 
-    return linked_plots
+
+def link_selection(*sources):
+    """
+    Sync the selected indices between multiple sources.
+    """
+    for i, _si in enumerate(sources):
+        for j, _sj in enumerate(sources):
+            if not i == j:
+                # link selection
+                _si.selected.js_link("indices", _sj.selected, "indices")
