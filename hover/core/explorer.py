6c6b77c19c1ee9ecdd5c40785a7ef2c4dbd14944
phurwicz <pavelhurwicz@gmail.com>
2020-11-26 01:26:57 -0500

    Significant refactorziation that reduces method overrides to attribute overrides as much as possible

--- hover/core/explorer.py
+++ hover/core/explorer.py
@@ -1,6 +1,7 @@
 """Interactive explorers mostly based on Bokeh."""
 import numpy as np
 from wasabi import msg as logger
+from bokeh.plotting import figure
 from bokeh.models import CustomJS, ColumnDataSource, CDSView, IndexFilter
 from bokeh.layouts import column, row
 from abc import ABC, abstractmethod
@@ -11,7 +12,7 @@ from .local_config import bokeh_hover_tooltip
 
 class BokehForLabeledText(ABC):
     """
-    Base class that keeps template figure settings.
+    Base class that keeps template explorer settings.
     """
 
     DEFAULT_FIGURE_KWARGS = {
@@ -37,6 +38,8 @@ class BokehForLabeledText(ABC):
         "output_backend": "webgl",
     }
 
+    DATA_KEYS = []
+
     def __init__(self, **kwargs):
         """
         Operations shared by all child classes.
@@ -47,16 +50,23 @@ class BokehForLabeledText(ABC):
         self.figure_settings = self.__class__.DEFAULT_FIGURE_KWARGS.copy()
         self.figure_settings.update(kwargs)
         self.reset_figure()
+        self.reset_data()
         self.setup_widgets()
 
     def reset_figure(self):
-        from bokeh.plotting import figure
-
+        """Start over on the figure."""
         self.figure = figure(**self.figure_settings)
+
+    def reset_data(self):
+        """Start over on data that comprise the figure."""
+        self.dfs = {_key: None for _key in self.__class__.DATA_KEYS}
+        self.sources = {_key: None for _key in self.__class__.DATA_KEYS}
 
     def setup_widgets(self):
         """
         Prepare widgets for interactive functionality.
+
+        Create positive/negative text search boxes.
         """
         from bokeh.models import TextInput
 
@@ -70,21 +80,18 @@ class BokehForLabeledText(ABC):
         )
 
     def layout_widgets(self):
-        """
-        Define the layout of widgets.
-        """
+        """Define the layout of widgets."""
         return column(self.search_pos, self.search_neg)
 
     def view(self):
-        """
-        Define the layout of the whole explorer.
-        """
+        """Define the layout of the whole explorer."""
         return column(self.layout_widgets(), self.figure)
 
     def activate_search(self, source, kwargs, altered_param=("size", 10, 5, 7)):
         """
         Enables string/regex search-and-highlight mechanism.
-        Modifies plotting source in-place.
+
+        Modifies the plotting source in-place.
         """
         assert isinstance(source, ColumnDataSource)
         assert isinstance(kwargs, dict)
@@ -177,10 +184,18 @@ class BokehCorpusExplorer(BokehForLabeledText):
     Plot unlabeled, 2-D-vectorized text data points in a corpus.
     """
 
+    DATA_KEYS = ["raw"]
+
     BACKGROUND_GLYPH_KWARGS = {
         "color": "gainsboro",
         "line_alpha": 0.3,
         "legend_label": "unlabeled",
+    }
+
+    SEARCH_FLAG_TO_PARAMS = {
+        "size": ("size", 10, 5, 7),
+        "fill_alpha": ("fill_alpha", 0.6, 0.4, 0.5),
+        "color": ("color", "coral", "linen", "gainsboro"),
     }
 
     def __init__(self, df_raw, **kwargs):
@@ -195,30 +210,26 @@ class BokehCorpusExplorer(BokehForLabeledText):
         # prepare plot-ready dataframe for train set
         for _key in ["text", "x", "y"]:
             assert _key in df_raw.columns
-        self.df_raw = df_raw.copy()
+        self.dfs["raw"] = df_raw.copy()
 
         # plot the train set as a background
         self.background_kwargs = self.__class__.BACKGROUND_GLYPH_KWARGS.copy()
-        self.source = ColumnDataSource(self.df_raw)
-        self._activate_search_on_corpus()
+        self.sources["raw"] = ColumnDataSource(self.dfs["raw"])
+        self._activate_search_builtin()
 
-    def _activate_search_on_corpus(self):
+    def _activate_search_builtin(self):
         """
-        Assuming that there will not be a labeled dev set.
+        Highlight positive search results and mute negative search results.
+
+        Note that this is a template method which heavily depend on class attributes.
         """
-        self.background_kwargs = self.activate_search(
-            self.source, self.background_kwargs, altered_param=("size", 10, 5, 7)
-        )
-        self.background_kwargs = self.activate_search(
-            self.source,
-            self.background_kwargs,
-            altered_param=("fill_alpha", 0.6, 0.4, 0.5),
-        )
-        self.background_kwargs = self.activate_search(
-            self.source,
-            self.background_kwargs,
-            altered_param=("color", "coral", "linen", "gainsboro"),
-        )
+        for _flag, _params in self.__class__.SEARCH_FLAG_TO_PARAMS.items():
+            logger.info(f"Activated {_flag} to respond to the search widgets.")
+            _params = self.__class__.SEARCH_FLAG_TO_PARAMS[_flag]
+            for _key in self.__class__.DATA_KEYS:
+                self.background_kwargs = self.activate_search(
+                    self.sources[_key], self.background_kwargs, altered_param=_params
+                )
 
     def plot(self, *args, **kwargs):
         """
@@ -226,7 +237,9 @@ class BokehCorpusExplorer(BokehForLabeledText):
         Called just once per instance most of the time.
         """
         self.reset_figure()
-        self.figure.circle("x", "y", source=self.source, **self.background_kwargs)
+        self.figure.circle(
+            "x", "y", source=self.sources["raw"], **self.background_kwargs
+        )
 
 
 class BokehCorpusAnnotator(BokehForLabeledText):
@@ -235,7 +248,9 @@ class BokehCorpusAnnotator(BokehForLabeledText):
     Annoate text data points via callbacks.
     """
 
-    def __init__(self, df_working, **kwargs):
+    DATA_KEYS = ["raw"]
+
+    def __init__(self, df_raw, **kwargs):
         """
         Requires the input dataframe to contain:
 
@@ -248,23 +263,25 @@ class BokehCorpusAnnotator(BokehForLabeledText):
         super().__init__(**kwargs)
 
         for _key in ["text", "x", "y"]:
-            assert _key in df_working.columns
-        self.df_working = df_working.copy()
-        if not "label" in self.df_working.columns:
-            self.df_working["label"] = module_config.ABSTAIN_DECODED
+            assert _key in df_raw.columns
+        self.dfs["raw"] = df_raw.copy()
+        if not "label" in self.dfs["raw"].columns:
+            self.dfs["raw"]["label"] = module_config.ABSTAIN_DECODED
 
-        self.source = ColumnDataSource(self.df_working)
+        self.sources["raw"] = ColumnDataSource(self.dfs["raw"])
         self.plot_kwargs = {"line_alpha": 0.3}
         self.reset_source()
         self.plot()
 
     def reset_source(self):
-        self.source.data = self.df_working
+        self.sources["raw"].data = self.dfs["raw"]
         self.plot_kwargs = self.activate_search(
-            self.source, self.plot_kwargs, altered_param=("size", 10, 5, 7)
+            self.sources["raw"], self.plot_kwargs, altered_param=("size", 10, 5, 7)
         )
         self.plot_kwargs = self.activate_search(
-            self.source, self.plot_kwargs, altered_param=("fill_alpha", 0.4, 0.05, 0.2)
+            self.sources["raw"],
+            self.plot_kwargs,
+            altered_param=("fill_alpha", 0.4, 0.05, 0.2),
         )
 
     def layout_widgets(self):
@@ -301,16 +318,19 @@ class BokehCorpusAnnotator(BokehForLabeledText):
         )
 
         def apply():
-            # a callback on clicking self.annotator_apply
-            # update labels in the source
+            """
+            A callback on clicking the 'self.annotator_apply' button.
+
+            Update labels in the source.
+            """
             label = self.annotator_input.value
-            selected_idx = self.source.selected.indices
+            selected_idx = self.sources["raw"].selected.indices
             if not selected_idx:
                 logger.warn("Did not select any data points.")
                 return
-            example_old = self.df_working.at[selected_idx[0], "label"]
-            self.df_working.at[selected_idx, "label"] = label
-            example_new = self.df_working.at[selected_idx[0], "label"]
+            example_old = self.dfs["raw"].at[selected_idx[0], "label"]
+            self.dfs["raw"].at[selected_idx, "label"] = label
+            example_new = self.dfs["raw"].at[selected_idx[0], "label"]
             logger.good(f"Updated DataFrame, e.g. {example_old} -> {example_new}")
 
             self.reset_source()
@@ -318,14 +338,18 @@ class BokehCorpusAnnotator(BokehForLabeledText):
             logger.good(f"Updated annotator plot at {current_time()}")
 
         def export():
-            # a callback on clicking self.annotator_export
+            """
+            A callback on clicking the 'self.annotator_export' button.
+
+            Saves the dataframe to a pickle.
+            """
             from dill import dump
             from datetime import datetime
 
             timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
             filename = f"bokeh-annotated-df-{timestamp}.pkl"
             with open(filename, "wb") as f:
-                dump(self.df_working, f)
+                dump(self.dfs["raw"], f)
             logger.good(f"Saved DataFrame to {filename}")
 
         self.annotator_apply.on_event(ButtonClick, apply)
@@ -334,10 +358,12 @@ class BokehCorpusAnnotator(BokehForLabeledText):
     def plot(self):
         """
         Re-plot with the new labels.
+
+        Determines the label->color mapping dynamically.
         """
         from bokeh.transform import factor_cmap
 
-        all_labels = sorted(set(self.df_working["label"].values), reverse=True)
+        all_labels = sorted(set(self.dfs["raw"]["label"].values), reverse=True)
         assert len(all_labels) <= 20, "Too many labels to support"
         cmap = "Category10_10" if len(all_labels) <= 10 else "Category20_20"
 
@@ -346,7 +372,7 @@ class BokehCorpusAnnotator(BokehForLabeledText):
             y="y",
             color=factor_cmap("label", cmap, all_labels),
             legend_field="label",
-            source=self.source,
+            source=self.sources["raw"],
             **self.plot_kwargs,
         )
 
@@ -358,11 +384,15 @@ class BokehMarginExplorer(BokehCorpusExplorer):
     Currently not considering multi-label scenarios.
     """
 
+    DATA_KEYS = ["raw"]
+
     BACKGROUND_GLYPH_KWARGS = {
         "color": "gainsboro",
         "line_alpha": 0.3,
         "fill_alpha": 0.0,
     }
+
+    SEARCH_FLAG_TO_PARAMS = {"size": ("size", 10, 5, 7)}
 
     def __init__(self, df_raw, label_col_a, label_col_b, **kwargs):
         """
@@ -374,17 +404,9 @@ class BokehMarginExplorer(BokehCorpusExplorer):
         super().__init__(df_raw, **kwargs)
 
         for _key in ["text", label_col_a, label_col_b, "x", "y"]:
-            assert _key in self.df_raw.columns
+            assert _key in self.dfs["raw"].columns
         self.label_col_a = label_col_a
         self.label_col_b = label_col_b
-
-    def _activate_search_on_corpus(self):
-        """
-        Overriding the parent method, because there will be labels.
-        """
-        self.background_kwargs = self.activate_search(
-            self.source, self.background_kwargs, altered_param=("size", 6, 1, 3)
-        )
 
     def plot(self, label, **kwargs):
         """
@@ -398,18 +420,21 @@ class BokehMarginExplorer(BokehCorpusExplorer):
         eff_kwargs["legend_label"] = f"{label}"
 
         # create agreement/increment/decrement subsets
-        col_a_pos = np.where(self.df_raw[self.label_col_a] == label)[0].tolist()
-        col_a_neg = np.where(self.df_raw[self.label_col_a] != label)[0].tolist()
-        col_b_pos = np.where(self.df_raw[self.label_col_b] == label)[0].tolist()
-        col_b_neg = np.where(self.df_raw[self.label_col_b] != label)[0].tolist()
+        col_a_pos = np.where(self.dfs["raw"][self.label_col_a] == label)[0].tolist()
+        col_a_neg = np.where(self.dfs["raw"][self.label_col_a] != label)[0].tolist()
+        col_b_pos = np.where(self.dfs["raw"][self.label_col_b] == label)[0].tolist()
+        col_b_neg = np.where(self.dfs["raw"][self.label_col_b] != label)[0].tolist()
         agreement_view = CDSView(
-            source=self.source, filters=[IndexFilter(col_a_pos), IndexFilter(col_b_pos)]
+            source=self.sources["raw"],
+            filters=[IndexFilter(col_a_pos), IndexFilter(col_b_pos)],
         )
         increment_view = CDSView(
-            source=self.source, filters=[IndexFilter(col_a_neg), IndexFilter(col_b_pos)]
+            source=self.sources["raw"],
+            filters=[IndexFilter(col_a_neg), IndexFilter(col_b_pos)],
         )
         decrement_view = CDSView(
-            source=self.source, filters=[IndexFilter(col_a_pos), IndexFilter(col_b_neg)]
+            source=self.sources["raw"],
+            filters=[IndexFilter(col_a_pos), IndexFilter(col_b_neg)],
         )
 
         to_plot = [
@@ -422,7 +447,7 @@ class BokehMarginExplorer(BokehCorpusExplorer):
         for _dict in to_plot:
             _view = _dict["view"]
             _marker = _dict["marker"]
-            _marker(*axes, source=self.source, view=_view, **eff_kwargs)
+            _marker(*axes, source=self.sources["raw"], view=_view, **eff_kwargs)
 
 
 class BokehSnorkelExplorer(BokehCorpusExplorer):
@@ -430,7 +455,14 @@ class BokehSnorkelExplorer(BokehCorpusExplorer):
     Plot text data points along with labeling function outputs.
     """
 
+    DATA_KEYS = ["raw", "labeled"]
+
     BACKGROUND_GLYPH_KWARGS = {"line_alpha": 0.6, "fill_alpha": 0.0, "size": 7}
+
+    SEARCH_FLAG_TO_PARAMS = {
+        "size": ("size", 10, 5, 7),
+        "fill_alpha": ("fill_alpha", 0.6, 0.0, 0.3),
+    }
 
     def __init__(self, df_raw, df_labeled, **kwargs):
         """
@@ -442,43 +474,35 @@ class BokehSnorkelExplorer(BokehCorpusExplorer):
         super().__init__(df_raw, **kwargs)
 
         # add 'label' column to df_raw
-        if not "label" in self.df_raw.columns:
-            self.df_raw["label"] = module_config.ABSTAIN_DECODED
+        if not "label" in self.dfs["raw"].columns:
+            self.dfs["raw"]["label"] = module_config.ABSTAIN_DECODED
 
-        # prepare plot-ready dataframe for dev set
+        # prepare plot-ready dataframe for df_labeled
         for _key in ["text", "label", "x", "y"]:
             assert _key in df_labeled.columns
-        self.df_labeled = df_labeled.copy()
+        self.dfs["labeled"] = df_labeled.copy()
+        self.sources["labeled"] = ColumnDataSource(self.dfs["labeled"])
 
-        # initialize a list of LFs to enable toggles
+        # initialize a list to keep track of plotted LFs
         self.lfs = []
-
-    def _activate_search_on_corpus(self):
-        """
-        Overriding the parent method, because there will be a labeled dev set.
-        """
-        self.background_kwargs = self.activate_search(
-            self.source, self.background_kwargs, altered_param=("size", 10, 5, 7)
-        )
-        self.background_kwargs = self.activate_search(
-            self.source,
-            self.background_kwargs,
-            altered_param=("fill_alpha", 0.6, 0.0, 0.3),
-        )
 
     def plot(self, lf, L_raw=None, L_labeled=None, include=("C", "I", "M"), **kwargs):
         """
         Plot a single labeling function.
+
         :param lf: labeling function decorated by @hover.utils.snorkel_helper.labeling_function()
+        :param L_raw: labeling function predictions, in decoded labels, on df_raw.
+        :param L_labeled: labeling function predictions, in decoded labels, on df_labeled.
+        :param include: subsets to show, which can be correct(C)/incorrect(I)/missed(M)/hit(H).
         """
         # keep track of added LF
         self.lfs.append(lf)
 
         # calculate predicted labels if not provided
         if L_raw is None:
-            L_raw = self.df_raw.apply(lf.row_to_label, axis=1).values
+            L_raw = self.dfs["raw"].apply(lf.row_to_label, axis=1).values
         if L_labeled is None:
-            L_labeled = self.df_labeled.apply(lf.row_to_label, axis=1).values
+            L_labeled = self.dfs["labeled"].apply(lf.row_to_label, axis=1).values
 
         # prepare plot settings
         axes = ("x", "y")
@@ -513,45 +537,42 @@ class BokehSnorkelExplorer(BokehCorpusExplorer):
         for _dict in to_plot:
             _view = _dict["view"]
             _marker = _dict["marker"]
-            _marker(*axes, source=self.source, view=_view, **eff_kwargs)
+            _marker(*axes, source=_view.source, view=_view, **eff_kwargs)
 
     def _view_correct(self, L_labeled):
         """
         Determine the subset correctly labeled by a labeling function.
         """
-
-        indices_raw = self.df_labeled["label"].values == L_labeled
-        indices = np.where(indices_raw)[0].tolist()
-        view = CDSView(source=self.source, filters=[IndexFilter(indices)])
+        agreed = self.dfs["labeled"]["label"].values == L_labeled
+        attempted = L_labeled != module_config.ABSTAIN_DECODED
+        indices = np.where(np.multiply(agreed, attempted))[0].tolist()
+        view = CDSView(source=self.sources["labeled"], filters=[IndexFilter(indices)])
         return view
 
     def _view_incorrect(self, L_labeled):
         """
         Determine the subset incorrectly labeled by a labeling function.
         """
-        disagreed = self.df_labeled["label"].values != L_labeled
+        disagreed = self.dfs["labeled"]["label"].values != L_labeled
         attempted = L_labeled != module_config.ABSTAIN_DECODED
-        indices_raw = np.multiply(disagreed, attempted)
-        indices = np.where(indices_raw)[0].tolist()
-        view = CDSView(source=self.source, filters=[IndexFilter(indices)])
+        indices = np.where(np.multiply(disagreed, attempted))[0].tolist()
+        view = CDSView(source=self.sources["labeled"], filters=[IndexFilter(indices)])
         return view
 
     def _view_missed(self, L_labeled, targets):
         """
         Determine the subset missed by a labeling function.
         """
-        targetable = np.isin(self.df_labeled["label"], targets)
+        targetable = np.isin(self.dfs["labeled"]["label"], targets)
         abstained = L_labeled == module_config.ABSTAIN_DECODED
-        indices_raw = np.multiply(targetable, abstained)
-        indices = np.where(indices_raw)[0].tolist()
-        view = CDSView(source=self.source, filters=[IndexFilter(indices)])
+        indices = np.where(np.multiply(targetable, abstained))[0].tolist()
+        view = CDSView(source=self.sources["labeled"], filters=[IndexFilter(indices)])
         return view
 
     def _view_hit(self, L_raw):
         """
         Determine the subset hit by a labeling function.
         """
-        indices_raw = L_raw != module_config.ABSTAIN_DECODED
-        indices = np.where(indices_raw)[0].tolist()
-        view = CDSView(source=self.source, filters=[IndexFilter(indices)])
+        indices = np.where(L_raw != module_config.ABSTAIN_DECODED)[0].tolist()
+        view = CDSView(source=self.sources["raw"], filters=[IndexFilter(indices)])
         return view
