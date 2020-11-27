a439d96aba3e48514f04775f70406746728e9cdd
phurwicz <pavelhurwicz@gmail.com>
2020-11-27 23:52:27 +0800

    Reworked @labeling_function so that it primarily works with Hover's functionalities and optionally links a Snorkel original

--- hover/utils/snorkel_helper.py
+++ hover/utils/snorkel_helper.py
@@ -1,29 +1,47 @@
-from snorkel.labeling import labeling_function as snorkel_lf
-from hover import module_params
+from snorkel.labeling import (
+    labeling_function as snorkel_lf,
+    LabelingFunction as SnorkelLF,
+)
 import uuid
 
 
-def labeling_function(targets, label_decoder, *args, **kwargs):
+def labeling_function(targets, label_encoder=None, **kwargs):
     """
-    Hover flavor of the Snorkel labeling_function decorator.
+    Hover's flavor of the Snorkel labeling_function decorator.
+
+    However, due to the dynamic label encoding nature of hover,
+    the decorated function should return the original string label, not its encoding integer.
 
     (1) assigns a UUID for easy identification;
     (2) keeps track of LF targets.
 
-    :param targets: labels that the labeling function is intended to create.
-    :type targets: list of int
-    :param label_decoder: {encoded_label -> decoded_label} mapping.
-    :type label_decoder: dict
+    - param targets(list of str): labels that the labeling function is intended to create.
+
+    - param label_encoder(dict): {decoded_label -> encoded_label} mapping.
+      - this can be omitted if you do NOT need a linked snorkel-style labeling function.
     """
 
     def wrapper(func):
-        lf = snorkel_lf(*args, **kwargs)(func)
+        # set up kwargs for Snorkel's LF
+        snorkel_kwargs = {"name": func.__name__}
+        snorkel_kwargs.update(kwargs)
+
+        # return value of hover's decorator
+        lf = SnorkelLF(f=func, **snorkel_kwargs)
+
+        # additional attributes
         lf.uuid = uuid.uuid1()
         lf.targets = targets[:]
-        lf.label_decoder = label_decoder
-        lf.row_to_label = lambda row: lf.label_decoder.get(
-            lf(row), module_params.ABSTAIN_DECODED
-        )
+
+        # link a snorkel-style labeling function if applicable
+        if label_encoder:
+            lf.label_encoder = label_encoder
+            snorkel_style_func = lambda x: lf.label_encoder[func(x)]
+            lf.snorkel = snorkel_lf(**kwargs)(snorkel_style_func)
+        else:
+            lf.label_encoder = None
+            lf.snorkel = None
+
         return lf
 
     return wrapper
