ced97b8d0a10d36bd40e6734178a2f78d9baef80
phurwicz <pavelhurwicz@gmail.com>
2020-12-11 22:49:51 +0800

    Added a Loggable class based on rich to facilitate templated logging

--- hover/core/__init__.py
+++ hover/core/__init__.py
@@ -5,3 +5,37 @@ Core module: contains classes which are centerpieces of the vast majority of use
 - explorer: defines high-level building blocks of the interactive UI.
 - neural: defines sub-applications that involve neural networks.
 """
+from rich.console import Console
+
+
+class Loggable:
+    """
+    Base class that provides consistently templated logging.
+
+    Inspired by wasabi's good/info/warn/fail methods.
+    """
+
+    CONSOLE = Console()
+
+    def _print(self, *args, **kwargs):
+        self.__class__.CONSOLE.print(*args, **kwargs)
+
+    def _good(self, message):
+        self.__class__.CONSOLE.print(
+            f":green_circle: {self.__class__.__name__}: {message}", style="green"
+        )
+
+    def _info(self, message):
+        self.__class__.CONSOLE.print(
+            f":blue_circle: {self.__class__.__name__}: {message}", style="blue"
+        )
+
+    def _warn(self, message):
+        self.__class__.CONSOLE.print(
+            f":yellow_circle: {self.__class__.__name__}: {message}", style="yellow"
+        )
+
+    def _fail(self, message):
+        self.__class__.CONSOLE.print(
+            f":red_circle: {self.__class__.__name__}: {message}", style="red"
+        )
