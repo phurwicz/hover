ba6b324f75247fa42ae8f3ecdf4e3b9b463891c8
phurwicz <pavelhurwicz@gmail.com>
2020-12-26 20:05:33 +0800

    Modified recipe tests, attempting to invoke handle functions

--- tests/recipes/test_experimental.py
+++ tests/recipes/test_experimental.py
@@ -1,18 +1,20 @@
 from hover.recipes.experimental import active_learning, snorkel_crosscheck
-from bokeh.io import curdoc
+from bokeh.document import Document
 
 
 def test_active_learning(
     mini_supervisable_text_dataset_embedded, dummy_vectorizer, dummy_vecnet_callback
 ):
     dataset = mini_supervisable_text_dataset_embedded.copy()
-    doc = curdoc()
-    active_learning(dataset, dummy_vectorizer, dummy_vecnet_callback)(doc)
+    doc = Document()
+    handle = active_learning(dataset, dummy_vectorizer, dummy_vecnet_callback)
+    handle(doc)
 
 
 def test_snorkel_crosscheck(
     mini_supervisable_text_dataset_embedded, dummy_labeling_function_list
 ):
     dataset = mini_supervisable_text_dataset_embedded.copy()
-    doc = curdoc()
-    snorkel_crosscheck(dataset, dummy_labeling_function_list)(doc)
+    doc = Document()
+    handle = snorkel_crosscheck(dataset, dummy_labeling_function_list)
+    handle(doc)
--- tests/recipes/test_stable.py
+++ tests/recipes/test_stable.py
@@ -1,14 +1,16 @@
 from hover.recipes import simple_annotator, linked_annotator
-from bokeh.io import curdoc
+from bokeh.document import Document
 
 
 def test_simple_annotator(mini_supervisable_text_dataset_embedded):
     dataset = mini_supervisable_text_dataset_embedded.copy()
-    doc = curdoc()
-    simple_annotator(dataset)(doc)
+    doc = Document()
+    handle = simple_annotator(dataset)
+    handle(doc)
 
 
 def test_linked_annotator(mini_supervisable_text_dataset_embedded):
     dataset = mini_supervisable_text_dataset_embedded.copy()
-    doc = curdoc()
-    linked_annotator(dataset)(doc)
+    doc = Document()
+    handle = linked_annotator(dataset)
+    handle(doc)
