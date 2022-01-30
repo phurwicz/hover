e72eaf97b5571834b1f0ab6abfda583e694691db
Pavel Hurwicz <pavelhurwicz@gmail.com>
2022-01-29 17:03:50 -0800

    Tests: added a test function for loading recipes in bokeh.server mode

---
+++ tests/recipes/test_servable.py
@@ -0,0 +1,26 @@
+from hover.recipes.stable import simple_annotator, linked_annotator
+from hover.recipes.experimental import active_learning, snorkel_crosscheck
+from bokeh.server.server import Server
+import time
+
+
+def test_builtin_servable_recipes(
+    mini_supervisable_text_dataset_embedded,
+    dummy_vecnet_callback,
+    dummy_labeling_function_list,
+):
+    dataset = mini_supervisable_text_dataset_embedded.copy()
+    simple = simple_annotator(dataset)
+    linked = linked_annotator(dataset)
+    active = active_learning(dataset, dummy_vecnet_callback)
+    snorkel = snorkel_crosscheck(dataset, dummy_labeling_function_list)
+    app_dict = {
+        "simple": simple,
+        "linked": linked,
+        "active": active,
+        "snorkel": snorkel,
+    }
+    server = Server(app_dict)
+    server.start()
+    time.sleep(20)
+    server.stop()
