a36e106ec72f4464fe7d389138afc40cb35f9269
Pavel <74003834+phurwicz@users.noreply.github.com>
2022-02-10 21:51:06 +0800

    Update test_bokeh_helper.py

--- tests/utils/test_bokeh_helper.py
+++ tests/utils/test_bokeh_helper.py
@@ -1,6 +1,7 @@
 from hover.utils.bokeh_helper import binder_proxy_app_url, remote_jupyter_proxy_url
 from urllib.parse import urlparse
 import pytest
+import os
 
 
 @pytest.mark.lite
@@ -8,8 +9,10 @@ def test_binder_proxy_app_url():
     """
     Not a full test, rather just validating urls.
     """
-    url = binder_proxy_app_url("simple-annotator")
+    os.environ["JUPYTERHUB_SERVICE_PREFIX"] = "hover-binder"
+    url = binder_proxy_app_url("simple-annotator", port=5007)
     _ = urlparse(url)
+    os.environ.pop("JUPYTERHUB_SERVICE_PREFIX")
 
 
 @pytest.mark.lite
