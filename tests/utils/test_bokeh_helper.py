63d37a24415d89eeb73a84268094058555a3ab9c
Pavel Hurwicz <pavelhurwicz@gmail.com>
2022-02-12 00:18:31 -0800

    Tests: dropped unnecessary lines now that binder_proxy_app_url uses os.environ.get()

--- tests/utils/test_bokeh_helper.py
+++ tests/utils/test_bokeh_helper.py
@@ -1,7 +1,6 @@
 from hover.utils.bokeh_helper import binder_proxy_app_url, remote_jupyter_proxy_url
 from urllib.parse import urlparse
 import pytest
-import os
 
 
 @pytest.mark.lite
@@ -9,17 +8,8 @@ def test_binder_proxy_app_url():
     """
     The function being tested is only intended for Binder.
     """
-    env_var_flag = "JUPYTERHUB_SERVICE_PREFIX" in os.environ
-    # preprocessing: pretend to be in Binder environment
-    if not env_var_flag:
-        os.environ["JUPYTERHUB_SERVICE_PREFIX"] = "hover-binder"
-
     url = binder_proxy_app_url("simple-annotator", port=5007)
     _ = urlparse(url)
-
-    # postprocessing: stop pretending to be in Binder environment
-    if not env_var_flag:
-        os.environ.pop("JUPYTERHUB_SERVICE_PREFIX")
 
 
 @pytest.mark.lite
