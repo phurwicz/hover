2286a5167213d0c2bf357991adb8a5971268ea3f
Pavel Hurwicz <pavelhurwicz@gmail.com>
2022-02-20 23:09:38 -0800

    Tests: added test for @servable exception handling

--- tests/utils/test_bokeh_helper.py
+++ tests/utils/test_bokeh_helper.py
@@ -1,6 +1,11 @@
-from hover.utils.bokeh_helper import binder_proxy_app_url, remote_jupyter_proxy_url
-from urllib.parse import urlparse
 import pytest
+from urllib.parse import urlparse
+from hover.utils.bokeh_helper import (
+    servable,
+    binder_proxy_app_url,
+    remote_jupyter_proxy_url,
+)
+from tests.recipes.local_helper import execute_handle_function
 
 
 @pytest.mark.lite
@@ -19,3 +24,16 @@ def test_remote_jupyter_proxy_url():
     """
     url = remote_jupyter_proxy_url(8888)
     _ = urlparse(url)
+
+
+@pytest.mark.lite
+def test_servable_wrapper(dummy_working_recipe, dummy_broken_recipe):
+    try:
+        dummy_broken_recipe()
+        pytest.fail("The dummy broken recipe above should have raised an exception.")
+    except AssertionError:
+        pass
+
+    for recipe in [dummy_working_recipe, dummy_broken_recipe]:
+        handle = servable()(recipe)
+        execute_handle_function(handle)
