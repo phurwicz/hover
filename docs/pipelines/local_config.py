66031563f38bcf61b29a8a260ade74b620baed04
Harry <haochuanwei@yahoo.com>
2022-07-23 08:53:23 -0700

    Docs pipeline: changed script names

--- docs/pipelines/local_config.py
+++ docs/pipelines/local_config.py
@@ -4,16 +4,16 @@ from markdown_include.include import MarkdownInclude
 
 DIR_PATH = os.path.dirname(__file__)
 NAME_TO_SCRIPT_REL = {
-    "tutorial-t0": "../pages/tutorial/t0-quickstart.md",
-    "tutorial-t1": "../pages/tutorial/t1-active-learning.md",
+    "t0-quickstart": "../pages/tutorial/t0-quickstart.md",
+    "t1-using-recipes": "../pages/tutorial/t1-active-learning.md",
     # tutorial-t2 has no script currently
-    "tutorial-t3": "../pages/tutorial/t3-dataset-population-selection.md",
-    "tutorial-t4": "../pages/tutorial/t4-annotator-dataset-interaction.md",
-    "tutorial-t5": "../pages/tutorial/t5-finder-filter.md",
-    "tutorial-t6": "../pages/tutorial/t6-softlabel-joint-filter.md",
-    "tutorial-t7": "../pages/tutorial/t7-snorkel-improvise-rules.md",
-    "guide-g0": "../pages/guides/g0-datatype-image.md",
-    "guide-g1": "../pages/guides/g1-datatype-audio.md",
+    "t3-dataset-mechanisms": "../pages/tutorial/t3-dataset-population-selection.md",
+    "t4-annotator-plot-tools": "../pages/tutorial/t4-annotator-dataset-interaction.md",
+    "t5-finder-selection-filter": "../pages/tutorial/t5-finder-filter.md",
+    "t6-soft-label-joint-filters": "../pages/tutorial/t6-softlabel-joint-filter.md",
+    "t7-custom-labeling-functions": "../pages/tutorial/t7-snorkel-improvise-rules.md",
+    "g0-image-data": "../pages/guides/g0-datatype-image.md",
+    "g1-audio-data": "../pages/guides/g1-datatype-audio.md",
 }
 NAME_TO_SCRIPT_ABS = {
     _k: os.path.join(DIR_PATH, _v) for _k, _v in NAME_TO_SCRIPT_REL.items()
